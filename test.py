2a96f6a1df6c1ad393c767d2cef07ac0ee005ccf
Martin Herbert <martinherbert93@gmail.com>
2020-05-04 04:55:26 -0400

    Working package, uploaded to pypi test.

--- test.py
+++
@@ -1,9 +0,0 @@
-#%%
-
-from lambdata_mherbert93 import mae_baseline
-import pandas
-
-df = pandas.DataFrame({"prices": [225, 250, 235, 500, 600], "feature1": [5, 6, 5, 10, 12], "feature2": [35, 37, 35, 60, 65]})
-
-print(df.head())
-#%%
