61102e4ae90b0428bdb18ef7268e4b9756a6e166
Martin Herbert <martinherbert93@gmail.com>
2020-05-05 16:13:26 -0400

    Refactored package to incorporate classes.

--- lambdata/lambdata_mherbert93.py
+++ lambdata/lambdata_mherbert93.py
@@ -1,18 +1,55 @@
-def mae_baseline(dataframe, target_col):
-    from sklearn.metrics import mean_absolute_error
+class Baseline:
+    def __init__(self, dataframe, target):
+        self.dataframe = dataframe
+        self.target = target
 
-    guess = dataframe[target_col].mean()
-    y_pred = [guess] * len(dataframe[target_col])
-    baseline_mae = mean_absolute_error(dataframe[target_col], y_pred)
+    def mae(self):
+        """
+        Used to calculate a MAE baseline based on the mean.
 
-    return baseline_mae
+        Parameters
+        ----------
+        dataframe: Expects pandas.DataFrame
 
-def accuracy_baseline(dataframe, target_col):
-    from sklearn.metrics import accuracy_score
-    majority_class = dataframe[target_col].mode()[0]
-    y_pred = [majority_class] * len(dataframe[target_col])
+        Returns
+        -------
+        Float
+        """
+        from sklearn.metrics import mean_absolute_error
 
-    return accuracy_score(dataframe[target_col], y_pred)
+        guess = self.dataframe[self.target].mean()
+        y_pred = [guess] * len(self.dataframe[self.target])
+        baseline_mae = mean_absolute_error(self.dataframe[self.target], y_pred)
+
+        return baseline_mae
+
+    def accuracy(self):
+        """
+        Used to calculate an accuracy baseline based on the majority class.
+
+        Parameters
+        ----------
+        dataframe: Expects pandas.DataFrame
+
+        Returns
+        -------
+        Float
+        """
+        from sklearn.metrics import accuracy_score
+        majority_class = self.dataframe[self.target].mode()[0]
+        y_pred = [majority_class] * len(self.dataframe[self.target])
+
+        return accuracy_score(self.dataframe[self.target], y_pred)
+
+if __name__ == "__main__":
+    import pandas as pd
+    df = pd.DataFrame({'target': ['True', 'True', 'True', 'True', 'False'], 'feature1': [5, 6, 4, 5, 5]})
+    baseline = Baseline(dataframe=df, target='target')
+    print("Baseline accuracy is: ", baseline.accuracy())
+
+    df = pd.DataFrame({'target': [250, 300, 225, 400, 600], 'feature1': [2, 3, 2, 5, 7]})
+    baseline = Baseline(dataframe=df, target='target')
+    print("Baseline MAE is: ", baseline.mae())
 
 
 
--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-mherbert93", # the name that you will install via pip
-    version="1.4",
+    version="1.6",
     author="Martin Herbert",
     author_email="martinherbert93@gmail.com",
-    description="Test package",
+    description="Not sure what I'd call this, but if I did, I'd do it here!",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     #license="MIT",
