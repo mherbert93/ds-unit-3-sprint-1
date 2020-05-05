da412341846b3dfcc355edf1216a3369071261fd
Martin Herbert <martinherbert93@gmail.com>
2020-05-04 20:19:15 -0400

    Update

--- build/lib/lambdata/lambdata_mherbert93.py
+++ build/lib/lambdata/lambdata_mherbert93.py
@@ -1,12 +1,18 @@
 def mae_baseline(dataframe, target_col):
     from sklearn.metrics import mean_absolute_error
-    target = 'total_cases'
 
     guess = dataframe[target_col].mean()
     y_pred = [guess] * len(dataframe[target_col])
     baseline_mae = mean_absolute_error(dataframe[target_col], y_pred)
 
-    return "Baseline MAE is: " + str(baseline_mae)
+    return baseline_mae
+
+def accuracy_baseline(dataframe, target_col):
+    from sklearn.metrics import accuracy_score
+    majority_class = dataframe[target_col].mode()[0]
+    y_pred = [majority_class] * len(dataframe[target_col])
+
+    return accuracy_score(dataframe[target_col], y_pred)
 
 
 
